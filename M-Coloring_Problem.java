6f1d32d174eb14ff0a7fb59ef9c2893a5684da7b
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-08-20 07:57:31 +0530

    Create M-Coloring_Problem.java

---
+++ M-Coloring_Problem.java
@@ -0,0 +1,68 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static boolean graphColoring(List <Integer>[] G, int []color, int i, int C)
+    {
+        int n = G.length;
+        if (solve(i,G,color,n,C) == true) return true;
+        return false;
+    }
+    private static boolean isSafe(int node, List<Integer>[] G, int []color, int n, int col) 
+    {
+        for(int it: G[node])
+        {
+            if(color[it] == col) return false;
+        }
+        return true;
+    }
+    private static boolean solve(int node, List<Integer>[] G,int []color,int n, int m)
+    {
+        if(node==n) return true;
+        
+        for(int i = 1; i <= m; i++)
+        {
+            if(isSafe(node, G, color, n, i)) {
+                color[node] = i;
+                if(solve(node + 1, G, color, n, m) == true) return true;
+                color[node] = 0;
+            }
+        }
+        return false;
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int N = 4, M = 2;
+		List<Integer> [] G = new ArrayList[N];
+		for (int i = 0; i < N; i++) 
+		{
+		    G[i] = new ArrayList <>();
+		}
+		G[0].add(1);
+		G[1].add(0);
+		G[1].add(2);
+		G[2].add(1);
+		G[2].add(3);
+		G[3].add(2);
+		G[3].add(0);
+		G[0].add(3);
+		G[0].add(2);
+		G[2].add(0);
+		int[] color = new int[N];
+		boolean ans = graphColoring(G, color, 0, M);
+		if(ans == true)
+		{
+		    System.out.println("1");
+		}
+		else 
+		{
+		    System.out.println("0");
+		}
+	}
+}
